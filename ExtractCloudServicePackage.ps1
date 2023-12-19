66ae05ed348f975060a715a5681a12b51a75cbbd
Nicholas McCollum <nimccoll@microsoft.com>
2023-12-19 08:12:53 -0500

    Initial check-in

---
+++ ExtractCloudServicePackage.ps1
@@ -0,0 +1,48 @@
+﻿#===============================================================================
+# Microsoft FastTrack for Azure
+# Extract the .cscfg file and .cspkg file for a classic Cloud Service to Azure
+# Storage
+#===============================================================================
+# Copyright © Microsoft Corporation.  All rights reserved.
+# THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY
+# OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT
+# LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+# FITNESS FOR A PARTICULAR PURPOSE.
+#===============================================================================
+param(
+    [Parameter(Mandatory)]$subscriptionId,
+    [Parameter(Mandatory)]$cloudServiceName,
+    [Parameter(Mandatory)]$deploymentName,
+    [Parameter(Mandatory)]$storageContainerUrl
+)
+# Login to Azure
+Connect-AzAccount
+
+# Set subscription context
+Set-AzContext -Subscription $subscriptionId
+
+# Get access token for the ARM management endpoint
+$accessToken = Get-AzAccessToken
+
+# Create Authorization header for the HTTP requests
+$authHeader = "Bearer " + $accessToken.Token
+$head = @{"Authorization"=$authHeader; "x-ms-version"="2012-03-01"; "ContentLength"="0"}
+
+# Define the validation endpoint URL and export endpoint URL
+$url = 'https://management.core.windows.net/' + $subscriptionId + '/services/hostedservices/' + $cloudServiceName + '/deployments/' + $deploymentName + '/package?containerUri=' + $storageContainerUrl + '&overwriteExisting=true'
+try {
+    Write-Host 'Attempting to extract .cscfg file and .cspkg file for classic Cloud Service' $cloudServiceName 'and deployment' $deploymentName -ForegroundColor Green
+    $getPackageResponse = Invoke-WebRequest -UseBasicParsing $url -Headers $head -Method POST
+    if ($getPackageResponse.StatusCode -eq '202') {
+        Write-Host '.cscfg file and .cspkg successfully downloaded to storage' -ForegroundColor Green
+    }
+    else {
+        Write-Host 'Failed to download .cscfg file and .cspkg file to storage' -ForegroundColor Red
+        Write-Host 'Status code is' $getPackageResponse.StatusCode -ForegroundColor Red
+        Write-Host $getPackageResponse.StatusDescription -ForegroundColor Red
+    }
+}
+catch {
+        Write-Host 'Failed to download .cscfg file and .cspkg file to storage' -ForegroundColor Red
+        Write-Host $PSItem.ToString() -ForegroundColor Red
+}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # ExtractCloudServiceDeployment
 Contains a sample PowerShell script to extract the .cscfg and .cspkg file from a classic Cloud Service
+
+# ExtractCloudServicePackage.ps1
+This script will allow you to extract the .cscfg file and .cspkg file from an existing classic Cloud Service. This may be useful in order to redeploy the Cloud Service to a different slot or to a different classic Cloud Service if you no longer have access to the source code.
+
+## Inputs
+- subscriptionId - Azure subscription ID
+- cloudServiceName - The name of the classic Cloud Service
+- deploymentName - The name of the deployment that will be extracted.
+- storageContainerUrl - The fully qualified URL of an Azure Storage Container where the .cscfg file and .cspkg file will be extracted.
+
+## Prerequisites
+- Contributor access to the classic Cloud Service and Azure Storage Account 
